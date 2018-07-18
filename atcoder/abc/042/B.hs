c13d0687d03862e18a5db57f53caa4e9f0365e63
show <honokani.kaoriga@gmail.com>
2018-07-18 01:13:29 +0000

    ADD abc042b

--- atcoder/abc/042/B.hs
+++ atcoder/abc/042/B.hs
@@ -1,10 +1,32 @@
-module Main
+module B
     ( main
     ) where
 
+import Data.List
+import Control.Monad
+
 main :: IO ()
 main = do
-    print "hi"
+    ss <- getLN_and_Chrs
+    putStrLn $ (concat.unfoldr takeMinStr) ss
 
+getLN_and_Chrs :: IO [String]
+getLN_and_Chrs = do
+    (l, n) <- mapT2 read.head.listToT2s.words <$> getLine
+    replicateM l $ take n.head <$> getWords
+    where
+        mapT2 :: (a -> b) -> (a,a) -> (b,b)
+        mapT2 f (a1,a2) = (f a1, f a2)
+        listToT2s :: [a] -> [(a,a)]
+        listToT2s [] = []
+        listToT2s (x:[]) = []
+        listToT2s (x:y:zs) = (x,y) : listToT2s zs
+        getWords :: IO [String]
+        getWords = words <$> getLine
 
+takeMinStr :: [String] -> Maybe (String, [String])
+takeMinStr [] = Nothing
+takeMinStr ss = Just (s, delete s ss)
+    where
+        s = minimum ss
 
